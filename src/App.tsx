eab803ea2c7fface7127cc5eff9a7d60414f73a6
RaulChavez <raul.chavez@bbrr.solutions>
2024-09-30 12:37:24 -0700

    Navbar styled!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/global.css";
+import "./components/Navbar.css";
 import { Routes, Route } from "react-router-dom";
 import { DotNet } from "./routes";
 import { GameDev } from "./routes";
@@ -8,25 +9,24 @@ import React, { ReactNode } from 'react';
 import Home from './routes/Home';
 
 function App() {
-	const MainApp = (): ReactNode => {
+	const MainApp = (): JSX.Element => {
 		return (
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/dotnet" element={<DotNet />} />
 				<Route path="/gamedev" element={<GameDev />} />
+				<Route path="/webdev" element={<GameDev />} />
 			</Routes>
 		);
 	}
 
 	return (
 		<>
-			<div className='row height-100'>
-				<div className='col-2 navbar-main-container'>
-					<Navbar />
-				</div>
-				<div className='col-9 main-app-container'>
-					<MainApp />
-				</div>
+			<div className='navbar-main-container position-fixed top-0 bottom-0 start-0'>
+				<Navbar />
+			</div>
+			<div className='main-app-container'>
+				<MainApp />
 			</div>
 		</>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom"
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const linkClasses = "no-link-nav d-flex nav-item align-items-center text-start";
+  const textClasses = "hidden-text nav-item-text";
+  const iconClasses = "nav-item-icon";
   return (
     <div>
-      <div className=""><Link to="/">Home</Link></div>
-      <div className=""><Link to="/dotnet">.Net</Link></div>
-      <div className=""><Link to="/gamedev">Game Dev</Link></div>
+      <div className="position-relative">
+        <Link className={`${linkClasses}`} to="/">
+          <p className={`${iconClasses} bi-house`}/><p className={`${textClasses}`}>{`Home`}</p>
+        </Link>
+      </div>
+      <div><Link className={`${linkClasses}`} to="/dotnet">
+        <p className={`${iconClasses} bi-microsoft`}/><p className={`${textClasses}`}>{`.NET`}</p>
+      </Link></div>
+      <div><Link className={`${linkClasses}`} to="/gamedev">
+        <p className={`${iconClasses} bi-controller`}/><p className={`${textClasses}`}>{`Game Dev`}</p>
+      </Link></div>
+      <div><Link className={`${linkClasses}`} to="/webdev">
+        <p className={`${iconClasses} bi-browser-chrome`}/><p className={`${textClasses}`}>{`Web Dev`}</p>
+      </Link></div>
     </div>
   )
 }
